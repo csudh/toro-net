dfa92ed0c4c8d80113779b1549eaf668c798988b
hugolucas <hugojlucas@gmail.com>
2017-10-08 15:44:14 -0700

    Front end validation for reg and login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,11 +2,13 @@
   <div v-if="!this.$store.state.user.displayName">
     <div class="container">
       <form id="login" action="/auth/local" method="post">
-        <div class="form-group">
-          <input class="form-control" type="email" name="email" placeholder="Email">
+        <div class="form-group" :class="{'has-error': errors.has('email') }" >
+          <input name="email" v-validate="'required|email'" data-vv-delay="500" type="text" data-vv-as="Email Address" placeholder="Email" class="form-control">
+          <p class="text-danger" align="left" v-if="errors.has('email')">{{ errors.first('email') }}</p>
         </div>
-        <div class="form-group">
-          <input type="password" class="form-control" name="password" placeholder="Password">
+        <div class="form-group" :class="{'has-error': errors.has('password') }" >
+          <input name="password" v-validate="'required'" data-vv-delay="500" type="text" data-vv-as="Password" placeholder="Password" class="form-control" type="password">
+          <p class="text-danger" align="left" v-if="errors.has('password')">{{ errors.first('password') }}</p>
         </div>
         <button class="btn btn-success" form="login" type="submit">Login</button>
       </form>
--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,25 +1,33 @@
 <template>
   <div class="container">
-    <div class="form-group">
-      <input type="text" class="form-control" placeholder="Display name" v-model="displayName">
-    </div>
-    <div class="form-group">
-      <input type="text" class="form-control" placeholder="Username" v-model="username">
-    </div>
-    <div class="form-group">
-      <input type="text" class="form-control" placeholder="Email" v-model="email">
-    </div>
-    <div class="form-group">
-      <input type="password" class="form-control" placeholder="Password" v-model="password">
-    </div>
-    <div class="form-group">
-      <input type="password" class="form-control" placeholder="Confirm password" v-model="passwordConf">
-    </div>
-    <button class="btn btn-success" @click="registerUser()">Register</button>
+    <form @submit.prevent="validateBeforeSubmit">
+      <div class="form-group" :class="{'has-error': errors.has('name') }" >
+        <input v-model="displayName" name="name" v-validate="'required|alpha'" data-vv-delay="500" type="text" data-vv-as="Display Name" placeholder="Name" class="form-control">
+        <p class="text-danger" align="left" v-if="errors.has('name')">{{ errors.first('name') }}</p>
+      </div>
+      <div class="form-group" :class="{'has-error': errors.has('username') }" >
+        <input v-model="username" name="username" v-validate="'required|alpha_dash'" data-vv-delay="500" type="text" data-vv-as="Username" placeholder="Username" class="form-control">
+        <p class="text-danger" align="left" v-if="errors.has('username')">{{ errors.first('username') }}</p>
+      </div>
+      <div class="form-group" :class="{'has-error': errors.has('email') }" >
+        <input v-model="email" name="email" v-validate="'required|email'" data-vv-delay="500" type="text" data-vv-as="Email Address" placeholder="Email" class="form-control">
+        <p class="text-danger" align="left" v-if="errors.has('email')">{{ errors.first('email') }}</p>
+      </div>
+      <div class="form-group" :class="{'has-error': errors.has('password') }" >
+        <input v-model="password" name="password"v-validate="{ rules: { regex: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, required: true} }" data-vv-delay="500" type="password" data-vv-as="Password" placeholder="Password" class="form-control">
+        <p class="text-danger" align="left" v-if="errors.has('password')">{{ errors.first('password') }}</p>
+      </div>
+      <div class="form-group" :class="{'has-error': errors.has('passwordconf') }" >
+        <input v-model="passwordConf" name="passwordconf" v-validate="'required|confirmed:password'" data-vv-delay="500" type="password" data-vv-as="Password Confirmation" placeholder="Confirm Password" class="form-control">
+        <p class="text-danger" align="left" v-if="errors.has('passwordconf')">{{ errors.first('passwordconf') }}</p>
+      </div>
+      <button class="btn btn-primary btn-block" type="submit">Submit</button>
+    </form>
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'Register',
   data() {
@@ -28,10 +36,23 @@ export default {
       username: '', 
       email: '',
       password: '',
-      passwordConf: '' 
+      passwordConf: '',
+      formSubmitted: false,
     }
   },
   methods: {
+    validateBeforeSubmit(e) {
+      console.log("Verifying...");
+      e.preventDefault();
+      this.$validator.validateAll().then((result) => {
+        if (result) {
+          // eslint-disable-next-line
+          alert('From Submitted!');
+          this.registerUser(); 
+          return;
+        } 
+      });
+    },
     registerUser() {
       const newUser = {
         displayName: this.displayName,
