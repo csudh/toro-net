fd71ddd27a50176d0f583f46a2a06b59513af570
Matthew Levan <matt.s.levan@gmail.com>
2017-10-10 14:16:10 -0700

    Updated data-vv-as fields to lowercase

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,11 +3,11 @@
     <div class="container">
       <form id="login" action="/auth/local" method="post">
         <div class="form-group" :class="{'has-error': errors.has('email') }" >
-          <input name="email" v-validate="'required|email'" data-vv-delay="500" type="text" data-vv-as="Email Address" placeholder="Email" class="form-control">
+          <input name="email" v-validate="'required|email'" data-vv-delay="500" type="text" data-vv-as="email address" placeholder="Email" class="form-control">
           <p class="text-danger" align="left" v-if="errors.has('email')">{{ errors.first('email') }}</p>
         </div>
         <div class="form-group" :class="{'has-error': errors.has('password') }" >
-          <input name="password" v-validate="'required'" data-vv-delay="500" type="text" data-vv-as="Password" placeholder="Password" class="form-control" type="password">
+          <input name="password" v-validate="'required'" data-vv-delay="500" type="text" data-vv-as="password" placeholder="Password" class="form-control" type="password">
           <p class="text-danger" align="left" v-if="errors.has('password')">{{ errors.first('password') }}</p>
         </div>
         <button class="btn btn-success" form="login" type="submit">Login</button>
--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,28 +1,33 @@
 <template>
   <div class="container">
-    <form @submit.prevent="validateBeforeSubmit" id="register">
-      <div class="form-group" :class="{'has-error': errors.has('name') }" >
-        <input v-model="displayName" name="name" v-validate="'required|alpha_spaces'" data-vv-delay="500" type="text" data-vv-as="display name" placeholder="Name" class="form-control">
-        <p class="text-danger" align="left" v-if="errors.has('name')">{{ errors.first('name') }}</p>
+    <div v-if="!this.$store.state.user.displayName">
+      <form @submit.prevent="validateBeforeSubmit" id="register">
+        <div class="form-group" :class="{'has-error': errors.has('name') }" >
+          <input v-model="displayName" name="name" v-validate="'required|alpha_spaces'" data-vv-delay="500" type="text" data-vv-as="display name" placeholder="Name" class="form-control">
+          <p class="text-danger" align="left" v-if="errors.has('name')">{{ errors.first('name') }}</p>
+        </div>
+        <div class="form-group" :class="{'has-error': errors.has('username') }" >
+          <input v-model="username" name="username" v-validate="'required|alpha_dash'" data-vv-delay="500" type="text" data-vv-as="username" placeholder="Username" class="form-control">
+          <p class="text-danger" align="left" v-if="errors.has('username')">{{ errors.first('username') }}</p>
+        </div>
+        <div class="form-group" :class="{'has-error': errors.has('email') }" >
+          <input v-model="email" name="email" v-validate="'required|email'" data-vv-delay="500" type="text" data-vv-as="email address" placeholder="Email" class="form-control">
+          <p class="text-danger" align="left" v-if="errors.has('email')">{{ errors.first('email') }}</p>
+        </div>
+        <div class="form-group" :class="{'has-error': errors.has('password') }" >
+          <input v-model="password" name="password"v-validate="{ rules: { regex: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, required: true} }" data-vv-delay="500" type="password" data-vv-as="password" placeholder="Password" class="form-control">
+          <p class="text-danger" align="left" v-if="errors.has('password')">{{ errors.first('password') }}</p>
+        </div>
+        <div class="form-group" :class="{'has-error': errors.has('passwordconf') }" >
+          <input v-model="passwordConf" name="passwordconf" v-validate="'required|confirmed:password'" data-vv-delay="500" type="password" data-vv-as="password confirmation" placeholder="Password confirmation" class="form-control">
+          <p class="text-danger" align="left" v-if="errors.has('passwordconf')">{{ errors.first('passwordconf') }}</p>
+        </div>
+        <button class="btn btn-primary" form="register" type="submit">Submit</button>
+      </form>
       </div>
-      <div class="form-group" :class="{'has-error': errors.has('username') }" >
-        <input v-model="username" name="username" v-validate="'required|alpha_dash'" data-vv-delay="500" type="text" data-vv-as="username" placeholder="Username" class="form-control">
-        <p class="text-danger" align="left" v-if="errors.has('username')">{{ errors.first('username') }}</p>
-      </div>
-      <div class="form-group" :class="{'has-error': errors.has('email') }" >
-        <input v-model="email" name="email" v-validate="'required|email'" data-vv-delay="500" type="text" data-vv-as="email address" placeholder="Email" class="form-control">
-        <p class="text-danger" align="left" v-if="errors.has('email')">{{ errors.first('email') }}</p>
-      </div>
-      <div class="form-group" :class="{'has-error': errors.has('password') }" >
-        <input v-model="password" name="password"v-validate="{ rules: { regex: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, required: true} }" data-vv-delay="500" type="password" data-vv-as="password" placeholder="Password" class="form-control">
-        <p class="text-danger" align="left" v-if="errors.has('password')">{{ errors.first('password') }}</p>
-      </div>
-      <div class="form-group" :class="{'has-error': errors.has('passwordconf') }" >
-        <input v-model="passwordConf" name="passwordconf" v-validate="'required|confirmed:password'" data-vv-delay="500" type="password" data-vv-as="password confirmation" placeholder="Password confirmation" class="form-control">
-        <p class="text-danger" align="left" v-if="errors.has('passwordconf')">{{ errors.first('passwordconf') }}</p>
-      </div>
-      <button class="btn btn-primary" form="register" type="submit">Submit</button>
-    </form>
+    <div v-else>
+      <h4>You must logout before you may register a new account!</h4>
+    </div>
   </div>
 </template>
 
@@ -47,21 +52,10 @@ export default {
       this.$validator.validateAll().then((result) => {
         if (result) {
           // eslint-disable-next-line
-          alert('From Submitted!');
-          this.registerUser(); 
+          alert('Form submitted!');
           return;
         } 
       });
-    },
-    registerUser() {
-      const newUser = {
-        displayName: this.displayName,
-        username: this.username,
-        email: this.email,
-        password: this.password,
-        passwordConf: this.passwordConf,
-      }
-      this.$store.dispatch('register', newUser)
     }
   }
 }
