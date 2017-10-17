d84ebb31d808f7136ffeede6c550a376fd58c3bb
ernesturzua <ernieurzua@gmail.com>
2017-10-17 00:10:26 -0700

    added vee validate package

--- dev-env-2.sh
+++ dev-env-2.sh
@@ -2,4 +2,5 @@
 sudo apt-get install -y mongodb-org &&
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash - &&
 sudo apt-get install -y nodejs &&
-npm install
+npm install &&
+npm install vee-validate
