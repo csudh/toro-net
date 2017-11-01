ed4d593774a3b54cb614d678332fa17ad20aeb6e
Ernest Urzua <ernieurzua@gmail.com>
2017-11-01 13:25:11 -0700

    hosted logo on imgur

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -14,7 +14,7 @@
   </div>
   <div class="container" v-else>
     <h4>Login to access Toro Net</h4>
-    <img src="https://lh6.googleusercontent.com/boHpliCk16WKnL--c78vBJgbQaElZMDYWgMB1AgLJGVbzS7_ZfRL6JQOIVobyjLBWSARv6MeSFMl94s=w1218-h955" alt="ToroNet logo">
+    <img src="https://imgur.com/a/Yee3F" alt="ToroNet logo">
     <!--<img src="src/components/assets/ToroNet.png" >-->
   </div>
 </template>
