0807779a72ebf22c5135de022a8153fd88c041c1
Carlos ontiveros <Caontiveros25@gmail.com>
2017-11-17 20:09:35 -0800

    added endpoint List all Posts Based on Keyword Search files:posts.js,package-lock.json

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -38,6 +38,27 @@ module.exports = (() => {
       })
     })
 
+
+
+        /*endpoint to provide partial list of posts based on keyword search*/
+    /* List all Posts Based on Keyword Search */
+    router.get('/list/:keyword', (req,res) => {
+      console.log("Listing posts who match the search")
+      //res.json({message:"User list here!"})
+      Post.find({body: {$regex: req.params.keyword}}, function(err, users) {
+        if (err) throw err;
+        var userMap = {};
+    
+        users.forEach(function(user) {
+          userMap[user._id] = user;
+        });
+    res.send(JSON.stringify(userMap));
+     
+      })
+    });
+
+    
+
     router.post('/create', (req, res) => {
       
       const newPost = new Post({
@@ -45,13 +66,12 @@ module.exports = (() => {
         title: req.body.title,
         body: req.body.body,
         createdOn: new Date
-      })
-    
-
+      })    
+      
+      console.log(req.body);  
+       
       
       
-      console.log(req.body);
-
       // Attempt to create the new user in the database.
       Post.create(newPost, (err) => {
         console.log(newPost)
