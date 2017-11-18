8f99a2136d42ce2e65b2f4c04e61dd1ec2c8ed28
Carlos ontiveros <Caontiveros25@gmail.com>
2017-11-17 20:15:52 -0800

    added more examples to run.sh file

--- run.sh
+++ run.sh
@@ -9,3 +9,16 @@ curl  http://localhost:3000/users/list/all
 # Replace test with any keyword
 curl  http://localhost:3000/users/list/test
 
+# Add/Creat Posts for Testing
+curl -d "user=test4@aol.com&title=Exam&body=final" -X POST http://localhost:3000/posts/create/
+
+curl -d "user=test4@aol.com&title=MidterExam&body=finalexam2" -X POST http://localhost:3000/posts/create/
+
+curl -d "user=test4@aol.com&title=FinalExamTitle&body=finalexam3" -X POST http://localhost:3000/posts/create/
+
+curl -d "user=test4@aol.com&title=ExamCSC501&body=csc501finalexam" -X POST http://localhost:3000/posts/create/
+
+# List all Posts Based on Keyword Search Example #
+curl http://localhost:3000/posts/list/exam
+
+
