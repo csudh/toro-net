25432fb6097b2dc941570448ddba78154c3cb214
hugoLucas <hugojlucas@gmail.com>
2017-11-21 16:49:22 -0800

    Cleaned up users.js and added comments

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,11 +2,15 @@ const express = require('express'),
       User = require('../models/user'),
       bcrypt = require('bcryptjs')
 
+/* apoc require statement must always go after the explicit loading of the 
+ * .env file */
+require('dotenv').load()
+const apoc = require('apoc')
+
 module.exports = (() => {
     'use strict'
 
     const router = express.Router()
-
 
     /* User registration API endpoint */
     router.post('/register', (req, res) => {
@@ -20,11 +24,6 @@ module.exports = (() => {
       
       /* Hash User password first and then create User object to store in DB on 
        * hash success */
-      
-      /* .env file must be explicity loaded here before the apoc module in order
-       *  to succesfully call neo4j */
-      require('dotenv').load()
-      const apoc = require('apoc')
       bcrypt.hash(req.body.password, 10, function(err, hash) {
         if (err) {
           res.status(499).send()
@@ -58,6 +57,7 @@ module.exports = (() => {
               throw err
             }
             
+            /* Creates a cypher query in order to add new user to friend graph */
             const queryString = `CREATE (u:User { username: "${req.body.username}" })`
             const query = apoc.query(queryString)
             query.exec().then((result) => {
@@ -78,11 +78,6 @@ module.exports = (() => {
       /* WARNING: Does not check to see if relationship is already established, may create 
       * duplicate friend relationship. */
       
-      /* .env file must be explicity loaded here before the apoc module in order
-      *  to succesfully call neo4j */
-      require('dotenv').load()
-      const apoc = require('apoc')
-      
       const queryString = 
         `MATCH (a:User),(b:User) 
         WHERE a.username = '${req.body.sUsername}' AND b.username = '${req.body.tUsername}'
@@ -90,22 +85,18 @@ module.exports = (() => {
       
       const query = apoc.query(queryString)
       query.exec().then((result) => {
-        /* Relationship created successfully */
-        console.log("SUCCESS", result)
         res.status(200).send()
       }, (fail) => {
-        /* Relationship not created */
-        console.log("ERROR", fail)
         res.status(500).send()
       })
     })
 
     /* Friend list API endpoint */
     router.get('/list/friend/:username/:degree', (req, res) => {
-      /* .env file must be explicity loaded here before the apoc module in order
-      *  to succesfully call neo4j */
-      require('dotenv').load()
-      const apoc = require('apoc')
+      /* Returns a list of friends for the username specified in the path. If the
+       * degree parameter is 2 it returns friends-of-friends and so on for larger 
+       * values. List returned does not include relationship details. 
+       */
 
       const queryString = 
         `MATCH (a:User), (b:User)
@@ -131,10 +122,8 @@ module.exports = (() => {
     
     /* Friend graph API endpoint */
     router.get('/list/friend/connections/:username/:degree', (req, res) => {
-      /* .env file must be explicity loaded here before the apoc module in order
-      *  to succesfully call neo4j */
-      require('dotenv').load()
-      const apoc = require('apoc')
+      /* Returns a JSON object where each key is a user in the friend graph and 
+       * each value is a list of users the key user is connected to. */
 
       const queryString = 
         `MATCH (u:User {username: '${req.params['username']}'})-[r:friend*1..${req.params['degree']}]-(v:User)
@@ -149,7 +138,6 @@ module.exports = (() => {
           for (var j = 0; j < dataList.length; j ++) {
             var rowNames = dataList[j]['connects'].split('<-->')
             rowNames = rowNames.sort()
-
             if ( resultMap.has(rowNames[0]) ) {
               if ( !resultMap.get(rowNames[0]).includes(rowNames[1]) ) {
                 resultMap.get(rowNames[0]).push(rowNames[1])
@@ -168,6 +156,7 @@ module.exports = (() => {
         console.log(fail) 
       })
     })
-  
-  return router;
+    
+    
+    return router;
 })();
