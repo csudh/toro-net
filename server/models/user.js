bb50d4f4a6ecdf7e205a8d58390be3dc0069cdfa
Matthew Levan <matt.s.levan@gmail.com>
2017-11-27 17:46:46 -0800

    Require all fields except password

--- server/models/user.js
+++ server/models/user.js
@@ -5,31 +5,27 @@ const mongoose = require('mongoose'),
 var User = new Schema({
   displayName: {
     type: String,
+    required: true
   },
   email: {
     type: String,
+    required: true,
+    unique: true,
     trim: true
   },
   username: {
     type: String,
+    required: true,
+    unique: true,
     trim: true
   },
   password: {
     type: String,
   },
-  question1: {
-    type: String,
-  },
-  question2: {
-    type: String,
-  },
-  question3: {
-    type: String,
-  },  
   createdOn: {
     type: Date,
+    required: true
   }
-  
 })
 
 module.exports = mongoose.model('User', User)
