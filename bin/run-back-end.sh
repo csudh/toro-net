a192b08bde0abdb61958b1a0d13972650319ed88
Matthew Levan <matt.s.levan@gmail.com>
2017-11-28 18:16:19 -0800

    Removed junk

--- bin/run-back-end.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-TOPDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../"
-mkdir -p $TOPDIR/var/log
-
-echo "starting backend server"
-cd $TOPDIR
-npm run serve > $TOPDIR/var/log/backend.log
--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const express = require('express'),
       mongoose = require('mongoose'),
       passport = require('passport'),
       session = require('express-session'),
+      cel = require('connect-ensure-login'),
       count = require('./server/routes/count'),
       auth = require('./server/routes/auth'),
       index = require('./server/routes/index'),
       users = require('./server/routes/users'),
       posts = require('./server/routes/posts')
-
-
+      MongoStore = require('connect-mongo')(session)
 
 require('dotenv').load();
 require('./passport')(passport)
@@ -27,10 +27,11 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(session({
-  secret: 'test-secret',
+  secret: 'toro-net',
   resave: false,
-  saveUninitialized: true,
-  cookie: { httpOnly: true, maxAge: 2495000000 }
+  saveUninitialized: false,
+  cookie: { httpOnly: true, maxAge: 2495000000 },
+  store: new MongoStore({ url: process.env.MONGO_URI })
 }))
 
 app.use(cookieParser('test-secret'))
@@ -42,6 +43,11 @@ app.use('/auth', auth),
 app.use('/count', count),
 app.use('/posts', posts),
 app.use('/', index)
+
+/* Catch all errors and log them. */
+app.use(function(err, req, res, next) {
+  console.log(err)
+})
 
 const port =  process.env.PORT || 3000;
 app.listen(port, () => console.log('Running on localhost:', port))
